1c7141dfb17deb3d6e118f5581230026a4786a84
Rita Freire <freire.rita@gmail.com>
2026-09-14 15:44:10 -0300

    aorganização de pastas

--- exercicios_while/senha.c
+++ exercicios_while/senha.c
@@ -3,7 +3,7 @@
 //Peça uma senha até que o usuário acerte (comparando com um valor fixo).
 
 #include <stdio.h> 
-#include <string.h>
+#include <stdlib.h>
 int main() {
 
     char senha[20] = "Rf8090@";
