25f47a520ebd331eaa41a2050139a4de35e24860
Rita Freire <freire.rita@gmail.com>
2026-09-16 11:33:57 -0300

    logica com js

---
+++ dio.logicadeprog/potes.js
@@ -0,0 +1,8 @@
+
+
+let poteCafe = "Café Pilão";
+let poteAcucar = "Açúcar Mascavo";
+let poteBiscoito = "Biscoito Passatempo";
+const mensagem = "Na cozinha tem ";
+
+console.log(mensagem + poteCafe + "," + poteAcucar + "," + "e " + poteBiscoito)
