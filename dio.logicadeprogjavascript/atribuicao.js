cd0d1367ac2ebec4b87db3183592998523209d3d
Rita Freire <freire.rita@gmail.com>
2026-09-21 16:09:30 -0300

    operações js

---
+++ dio.logicadeprogjavascript/atribuicao.js
@@ -0,0 +1,6 @@
+
+
+let preco = 10
+preco += 5  // preco = preco + 5
+preco -= 5 // preco = preco -5
+console.log(preco)
---
+++ dio.logicadeprogjavascript/incrementodecremento.js
@@ -0,0 +1,13 @@
+
+
+let contador = 1
+// '++' incremento
+contador++
+contador++
+
+// '--' decremento
+contador--
+contador--
+contador--
+
+console.log(contador)
