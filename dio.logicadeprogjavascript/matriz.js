7c704aae422fbc9e1e71e6865535afdfe76ef15b
Rita Freire <freire.rita@gmail.com>
2026-09-21 10:46:33 -0300

    java exercicios

---
+++ dio.logicadeprogjavascript/matriz.js
@@ -0,0 +1,20 @@
+
+//vetor: array
+
+let nomesPokemon = ["Pikachu", "Charmander ", "Bubasaur"]
+
+//matriz
+
+let timePokemon = [
+    ["Pikachu", "M" , "level 1"],
+    ["Charmander", "F" , "level 3"],
+    ["Bubasaur", "M" , "level 4"],
+
+
+//Se quiser pegar apenas o nome do primeiro Pokémon
+//Porque o primeiro [0] significa primeiro Pokémon e o segundo [0] significa primeiro elemento desse Pokémon
+//funciona como linha e coluna X e Y
+
+];
+
+console.log("O pokemon " + timePokemon[0][0] + "é do sexo" + timePokemon[0][1]);
