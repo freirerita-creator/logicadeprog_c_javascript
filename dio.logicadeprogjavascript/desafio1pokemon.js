5207030a15194dba0e7b4dcdd25243d4e6e5e94f
Rita Freire <freire.rita@gmail.com>
2026-09-24 15:10:54 -0300

    desafio1

---
+++ dio.logicadeprogjavascript/desafio1pokemon.js
@@ -0,0 +1,25 @@
+
+// Crie um código para armazenar o máximo de informações possível dos pokemon da imagem ao lado e ao final, para cada um, exiba uma mensagem de saída escrita o nome do pokemon concatenado com "Cadastrado com sucesso"
+
+
+
+
+//Se quiser pegar apenas o nome do primeiro Pokémon
+//Porque o primeiro [0] significa primeiro Pokémon e o segundo [0] significa primeiro elemento desse Pokémon
+//funciona como linha e coluna X e Y
+
+
+
+let timePokemon = [
+    ["Poochyena", "Level 3"],
+    ["Zigzagoo", "Level 2"],
+    ["Dragonite", "Level 5"],
+
+]
+    
+console.log("O pokemon " + timePokemon[1][0] + "tem " + timePokemon[1][2])
+
+
+
+
+
